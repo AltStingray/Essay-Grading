88f428a047501bae2b9f5163d7eed9d8dbdb8f38
Alt <egoroff2206@gmail.com>
2024-10-07 18:52:32 +0100

    Deploy Summary Report Generator to Git

---
+++ assemblyAI.py
@@ -0,0 +1,20 @@
+import assemblyai as aai
+
+aai.settings.api_key = "bc90a68cad05489689f948691bc5d3de"
+
+FILE_URL = "audio.mp3"
+
+def run():
+    config = aai.TranscriptionConfig(speaker_labels=True)
+
+    transcriber = aai.Transcriber()
+    transcript =transcriber.transcribe(
+        FILE_URL,
+        config=config   
+    )
+
+    result = ""
+    for utterance in transcript.utterances:
+        result+= (f"\nSpeaker: {utterance.speaker}: {utterance.text}\n")
+
+    return result
---
+++ dropbox_module.py
@@ -0,0 +1,58 @@
+import dropbox
+import requests
+
+CLIENT_ID = "shc5rkn4hixve4j"
+CLIENT_SECRET = "wejv8edxnhyelum"
+
+redirect_link = f"https://www.dropbox.com/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:8080/start&response_type=code"
+
+data = {}
+
+def store(value, access_token_or_link):
+
+    data[access_token_or_link] = value
+
+
+def authorization(auth_code):
+    token_url = "https://api.dropboxapi.com/oauth2/token"
+
+    response = requests.post(
+        token_url,
+        data={
+            "code": auth_code,
+            "grant_type": "authorization_code",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "redirect_uri": "http://localhost:8080/start"
+        }
+    )
+
+    if response.status_code==200:
+        token_data = response.json()
+        access_token = token_data["access_token"]
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
+    return access_token
+
+
+def download_file():
+
+    access_token = data["access_token"]
+    
+    url = data["link"]
+
+    dbx = dropbox.Dropbox(access_token, app_key="shc5rkn4hixve4j" , app_secret="wejv8edxnhyelum")
+
+    with open("video.mp3", 'wb') as file:
+
+        try:
+            metadata, res = dbx.sharing_get_shared_link_file(url)
+            file.write(res.content)
+            print("File downloaded successfully!")
+
+        except dropbox.exceptions.ApiError as err:
+            print(f"Error: {err}")
+
+    return file
