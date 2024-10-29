6bc3e9aaf0216cecc5dbaa1cbbb6217f56afc7ea
Alt <egoroff2206@gmail.com>
2024-10-29 16:55:52 +0000

    Fix

--- db_postgres.py
+++ db_postgres.py
@@ -37,7 +37,7 @@ def db(command):
 
         #cursor.execute("""ALTER TABLE Logs ADD filename VARCHAR(255)""")
 
-        cursor.execute("""ALTER SEQUENCE Logs RESTART WITH 4""")
+        cursor.execute("""ALTER SEQUENCE id RESTART WITH 4""")
         #"""SELECT setval('id', 2)"""
 
         db_conn.commit()
