a116a9044ca08e6cb8dcc2299b636c7f18562f57
Alt <egoroff2206@gmail.com>
2024-10-31 17:45:58 +0000

    Fix

--- main.py
+++ main.py
@@ -178,22 +178,23 @@ def download():
     else:
         strip_summary = result[0].replace("```\n", "").replace("```", "").strip()
 
-    summary_report = retrieve(json.loads(strip_summary))
+    summary_report_text = retrieve(json.loads(strip_summary)["text"])
+    summary_report_html = strip_summary["html"].tobytes().decode('utf-8').strip("{ }")
     transcription = retrieve(result[1])
     filename = result[2]
 
     pick_one = request.args.get("pick_one")
 
     if pick_one == "Summary report.odt":
-        return send_file(summary_report["text"], as_attachment=True, download_name=f"summary_report_{filename}.odt", mimetype="application/vnd.oasis.opendocument.text")
+        return send_file(summary_report_text, as_attachment=True, download_name=f"summary_report_{filename}.odt", mimetype="application/vnd.oasis.opendocument.text")
     elif pick_one == "Transcription.odt":
         return send_file(transcription, as_attachment=True, download_name=f"transcription_{filename}.odt", mimetype="application/vnd.oasis.opendocument.text")
     elif pick_one == "Summary report.docx":
-        return send_file(summary_report["text"], as_attachment=True, download_name=f"summary_report_{filename}.docx", mimetype="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+        return send_file(summary_report_text, as_attachment=True, download_name=f"summary_report_{filename}.docx", mimetype="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
     elif pick_one == "Transcription.docx":
         return send_file(transcription, as_attachment=True, download_name=f"transcription_{filename}.docx", mimetype="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
     elif pick_one == "Summary report preview":
-        return render_template("preview_report.html", html=summary_report["html"])
+        return render_template("preview_report.html", html=summary_report_html)
 
 @app.route('/history')
 def history():
