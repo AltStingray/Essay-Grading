4834aa4d888475f2ba9f83c629f7bb29b595c0de
Alt <egoroff2206@gmail.com>
2024-11-08 22:50:11 +0000

    FINALLLYYY

--- db_postgres.py
+++ db_postgres.py
@@ -107,14 +107,14 @@ def db_store(data, db_name):
     db_conn = psycopg2.connect(DATABASE)
 
     cursor = db_conn.cursor()
-    elements_essay = ''.join("topic, essay, paragraphs_count, words_count, grammar_mistakes, linking_words_count, repetative_words_count, submitted_by, overall_band_score, sidebar_comments, time")
-    for n, i in enumerate(data.values()):
+
+    for i in data.values():
         if db_name == "logs":
             insert_sql = f"INSERT INTO {db_name}(summary, transcription, filename, summary_html) VALUES{i};"
         else:
-            insert_sql = f"INSERT INTO {db_name}({elements_essay[n]}) VALUES({i});"
+            insert_sql = f"INSERT INTO {db_name}(topic, essay, paragraphs_count, words_count, grammar_mistakes, linking_words_count, repetative_words_count, submitted_by, overall_band_score, sidebar_comments, time) VALUES({i});"
     
-    cursor.execute(insert_sql)
+            cursor.execute(insert_sql)
 
     db_conn.commit()
 
