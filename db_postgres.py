b3e9fd6918182c21be3ab1dbebe32f5a9d1fe7c6
Alt <egoroff2206@gmail.com>
2024-11-08 22:38:24 +0000

    Fix

--- db_postgres.py
+++ db_postgres.py
@@ -107,12 +107,12 @@ def db_store(data, db_name):
     db_conn = psycopg2.connect(DATABASE)
 
     cursor = db_conn.cursor()
-
-    for i in data.values():
+    elements_essay = ''.join("topic, essay, paragraphs_count, words_count, grammar_mistakes, linking_words_count, repetative_words_count, submitted_by, overall_band_score, sidebar_comments, time")
+    for n, i in enumerate(data.values()):
         if db_name == "logs":
             insert_sql = f"INSERT INTO {db_name}(summary, transcription, filename, summary_html) VALUES{i};"
         else:
-            insert_sql = f"INSERT INTO {db_name}(topic, essay, paragraphs_count, words_count, grammar_mistakes, linking_words_count, repetative_words_count, submitted_by, overall_band_score, sidebar_comments, time) VALUES({i});"
+            insert_sql = f"INSERT INTO {db_name}({elements_essay[n]}) VALUES({i});"
     
     cursor.execute(insert_sql)
 
