0809d55ba6d5de836e9b4ca878abc2f20876d458
Alt <egoroff2206@gmail.com>
2024-11-19 19:07:04 +0000

    Test

--- db_postgres.py
+++ db_postgres.py
@@ -76,7 +76,7 @@ def db(command):
     elif command == "delete_data":
 
         #cursor.execute(f"DELETE FROM Logs WHERE id IN ('6')")
-        cursor.execute(f"DELETE FROM essay_logs WHERE id IN ('1')")
+        cursor.execute(f"DELETE FROM essay_logs WHERE id IN ('1', '2', '3', '4', '5')")
 
         db_conn.commit()
 
