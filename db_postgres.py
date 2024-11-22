1b3baba0db297b867de72e167893ae709016b7d4
Alt <egoroff2206@gmail.com>
2024-11-22 23:48:55 +0000

    Fix

--- db_postgres.py
+++ db_postgres.py
@@ -124,7 +124,7 @@ def sent_email(id):
     cursor = db_conn.cursor()
 
     cursor.execute(f"UPDATE Logs SET sent = 'True' WHERE id = {id}")
-    cursor.execute(f"UPDATE Logs SET sent = 'None' WHERE id = 2")
+    cursor.execute(f"UPDATE Logs SET sent = 'False' WHERE id = 2")
 
     db_conn.commit()
 
