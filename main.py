19e11cb2e728c9e724700c7b3147c850b966a2d8
Alt <egoroff2206@gmail.com>
2024-11-26 19:21:14 +0000

    Fix

--- main.py
+++ main.py
@@ -459,8 +459,8 @@ def grading_queue():
         {"instruction": introduction},
         {"steps": []}
     ]
-    #[prompt_1, prompt_2, prompt_3, prompt_4, band_score]
-    for one in [prompt_1]:
+
+    for one in [prompt_1, prompt_2, prompt_3, prompt_4, band_score]:
         d = {"role": "user", "content": one}
         prompt[1]["steps"].append(d)
 
@@ -714,6 +714,7 @@ def RunOpenAI(prompt, content, tool):
         ]
 
         for step in prompt[1]["steps"]:
+            step = step["content"].strip("\n ")
             print(step)
             messages.append(step)
 
