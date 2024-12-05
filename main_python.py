7bb1214f31a0666295340325e06b8fa4887b2c41
Alt <egoroff2206@gmail.com>
2024-12-05 14:30:33 +0000

    Fix

--- main_python.py
+++ main_python.py
@@ -123,7 +123,7 @@ def main_summary_report(link, specified_date, teacher, client_name, client_email
 
     print("Transcription created, working on the summary report...")
 
-    if specified_date == None or specified_date == "" or len(specified_date) == 0:
+    if specified_date == None or specified_date == "" or len(str(specified_date)) == 0:
         specified_date = datetime.now().strftime("%d-%m-%Y")
 
     if user_prompt != None:
