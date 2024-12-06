50bb1601e6f8a5454b483734161fe24620788ddf
Alt <egoroff2206@gmail.com>
2024-12-06 20:30:09 +0000

    missed letter s in essay_logS

--- main.py
+++ main.py
@@ -487,7 +487,7 @@ def view_logs(id):
         essay = essay_grammar_mistakes
         words_category = "Grammar Mistakes"
 
-    return render_template('grading.html', name="finish", topic=topic, essay=essay, paragraphs_count=paragraphs_count, words_count=words_count, corrected_words=sidebar_comments, submitted_by=submitted_by, current_date=current_date, linking_words_count=linking_words_count, repetitive_words_count=repetitive_words_count, grammar_mistakes_count=grammar_mistakes_count, band_score=band_score, unnecessary_words_count=unnecessary_words_count, id=id, words_category=words_category, route="view_log")
+    return render_template('grading.html', name="finish", topic=topic, essay=essay, paragraphs_count=paragraphs_count, words_count=words_count, corrected_words=sidebar_comments, submitted_by=submitted_by, current_date=current_date, linking_words_count=linking_words_count, repetitive_words_count=repetitive_words_count, grammar_mistakes_count=grammar_mistakes_count, band_score=band_score, unnecessary_words_count=unnecessary_words_count, id=id, words_category=words_category, route="view_logs")
 
 
 
